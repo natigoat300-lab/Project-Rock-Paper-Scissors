f3b55fe1bd1ea814640a7bdb2d06dda291f040c3
natigoat37 <natigoat300@gmail.com>
2026-04-26 11:05:44 +0300

    feat: add function for random computer choic

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+// creat a fucniton for computer choice
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3)
+}
+// make it select randomly a number
+// make it to choice randommly a rock paper sicsorrs
